b649d1ae4e71a7ac6e1040703047f117afbe1315
Soulike <316413310@qq.com>
2019-03-31 15:36:49 +0800

    添加 Modal

--- src/Store.js
+++ src/Store.js
@@ -2,11 +2,15 @@ import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import thunk from 'redux-thunk';
 // import 所有 Reducer
 import {Reducer as AuthProcessorReducer} from './Components/AuthProcessor';
+import {Reducer as ModalReducer} from './ComponentContainer/Modal';
 
 // Store 中的初始值，根据开发需要进行改变
 const initValues = {
     AuthProcessor: {
         hasLoggedIn: false,
+    },
+    Modal: {
+        currentVisibleModalIdSet: new Set(),
     },
 };
 
@@ -20,6 +24,7 @@ const storeEnhancers = compose(
 // 所有 Reducer 放在此处
 const Reducer = combineReducers({
     AuthProcessor: AuthProcessorReducer,
+    Modal: ModalReducer,
 });
 
 export default createStore(Reducer, initValues, storeEnhancers);
