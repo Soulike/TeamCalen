266feaa8e46e39ef8834978858881932a0cfc4c5
Soulike <316413310@qq.com>
2019-04-15 22:45:21 +0800

    拆分刷新函数

--- src/Pages/ControlPanel/NewSchedule/Components/ScheduleOverview/Components/ScheduleModal/Container.js
+++ src/Pages/ControlPanel/NewSchedule/Components/ScheduleOverview/Components/ScheduleModal/Container.js
@@ -13,107 +13,65 @@ import {
 import {updateScheduleInfo} from '../../../../Function';
 import MODAL_ID from '../../../../../../../CONSTANT/MODAL_ID';
 
-// import SCHEDULE_STATE from '../../../../../../../CONSTANT/SCHEDULE_STATE';
-
 class ScheduleModalContainer extends React.Component
 {
     constructor(props)
     {
         super(props);
         this.state = {
-            timelineItems: [
-                /*{
-                    month: '03',
-                    day: '31',
-                    startHour: 8,
-                    startMinute: 10,
-                    endHour: 12,
-                    endMinute: 30,
-                    scheduleText: '英语作业啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊',
-                    scheduleState: SCHEDULE_STATE.CANCELED,
-                },
-                {
-                    month: '03',
-                    day: '31',
-                    startHour: 12,
-                    startMinute: 30,
-                    endHour: 14,
-                    endMinute: 30,
-                    scheduleText: '高数作业',
-                    scheduleState: SCHEDULE_STATE.FINISHED,
-                },
-                {
-                    day: '31',
-                    startHour: 14,
-                    startMinute: 30,
-                    endHour: 18,
-                    endMinute: 30,
-                    scheduleText: '英语作业',
-                    scheduleState: SCHEDULE_STATE.UNFINISHED,
-                },*/
-            ],
+            schedules: [],
         };
     }
 
-    setStateAsync = async state =>
+    refreshSchedules = async () =>
     {
-        return new Promise(resolve =>
+        updateScheduleInfo();
+        const {year, month, day} = this.props;
+        const schedulesWrapper = await Api.sendGetSchedulesByDayRequestAsync(year, month, day);
+        if (schedulesWrapper)
         {
-            this.setState(state, resolve);
-        });
-    };
-
-    refreshSchedulesFactory = () =>
-    {
-        return async () =>
-        {
-            updateScheduleInfo();
-            const {year, month, day} = this.props;
-            const schedulesWrapper = await Api.sendGetSchedulesByDayRequestAsync(year, month, day);
             if (schedulesWrapper)
             {
                 const {schedules} = schedulesWrapper;
-                const timelineItems = [];
-                schedules.forEach(schedule =>
-                {
-                    const {
-                        id,
-                        month,
-                        day,
-                        startHour,
-                        startMinute,
-                        endHour,
-                        endMinute,
-                        scheduleText,
-                        scheduleState,
-                    } = schedule;
-                    timelineItems.push(
-                        new TimelineItemObject.TimelineItem(id, month, day, startHour, startMinute, endHour, endMinute,
-                            scheduleText, scheduleState,
-                            onSwitchChangeFactory(id, this.refreshSchedulesFactory()),
-                            onResumeClickFactory(id, this.refreshSchedulesFactory()),
-                            onCancelClickFactory(id, this.refreshSchedulesFactory()),
-                            onDeleteClickFactory(id, this.refreshSchedulesFactory()),
-                            onModifyClickFactory(id, MODAL_ID.SCHEDULE_MODIFY_MODAL_FOR_SCHEDULE_MODAL, this.refreshSchedulesFactory()),
-                            // TODO: BUG，第一次事件没有挂上
-                        ),
-                    );
-                });
-                await this.setStateAsync({timelineItems});
+                this.setState(schedules);
             }
-        };
+        }
     };
 
     onShow = async () =>
     {
-        await this.setStateAsync({timelineItems: []});
-        await (this.refreshSchedulesFactory())();
+        await this.refreshSchedules();
     };
 
     render()
     {
         const {year, month, day} = this.props;
-        const {timelineItems} = this.state;
+        const {schedules} = this.state;
+        const timelineItems = [];
+        schedules.forEach(schedule =>
+        {
+            const {
+                id,
+                month,
+                day,
+                startHour,
+                startMinute,
+                endHour,
+                endMinute,
+                scheduleText,
+                scheduleState,
+            } = schedule;
+            timelineItems.push(
+                new TimelineItemObject.TimelineItem(id, month, day, startHour, startMinute, endHour, endMinute,
+                    scheduleText, scheduleState,
+                    onSwitchChangeFactory(id, this.refreshSchedules),
+                    onResumeClickFactory(id, this.refreshSchedules),
+                    onCancelClickFactory(id, this.refreshSchedules),
+                    onDeleteClickFactory(id, this.refreshSchedules),
+                    onModifyClickFactory(id, MODAL_ID.SCHEDULE_MODIFY_MODAL_FOR_SCHEDULE_MODAL, this.refreshSchedules),
+                ),
+            );
+        });
         return (
             <ScheduleModal year={year}
                            month={month}
