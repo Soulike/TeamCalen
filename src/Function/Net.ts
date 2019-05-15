652dbdc3c1abec8001a78b0d9c1c6f52984de408
Soulike <316413310@qq.com>
2019-05-15 10:42:40 +0800

    为函数添加注释

--- src/Function/Net.ts
+++ src/Function/Net.ts
@@ -1,6 +1,13 @@
-import axios from 'axios';
-import {AxiosRequestConfig} from 'axios';
+import axios, {AxiosRequestConfig} from 'axios';
 
+/**
+ * @description dispatch http get request to specific url
+ * @param {string} url - target url
+ * @param {boolean} allowCache - whether allow browser to use cache for the request
+ * @param params - items listed in query string
+ * @param {AxiosRequestConfig} config - config of the request
+ * @return {Promise<any>} - Data that server responses
+ * */
 export async function getAsync(url: string, allowCache: boolean = true, params = {}, config: AxiosRequestConfig = {}) : Promise<any>
 {
     return new Promise(async (resolve, reject) =>
@@ -21,9 +28,15 @@ export async function getAsync(url: string, allowCache: boolean = true, params =
             reject(e);
         }
     });
-
 }
 
+/**
+ * @description dispatch http post request to specific url
+ * @param {string} url - target url
+ * @param params - items listed in request body
+ * @param {AxiosRequestConfig} config - config of the request
+ * @return {Promise<any>} - Data that server responses
+ * */
 export async function postAsync(url: string, params = {}, config: AxiosRequestConfig = {}): Promise<any>
 {
     return new Promise(async (resolve, reject) =>
--- src/Function/Url.ts
+++ src/Function/Url.ts
@@ -1,9 +1,15 @@
+/**
+ * @description add prefix for specific url path
+ * */
 export function requestPrefix(url: string): string
 {
     url = removePrependSlashes(url);
     return `/server/${url}`;
 }
 
+/**
+ * @description remove prepend slashes from specific str
+ * */
 export function removePrependSlashes(str: string): string
 {
     while (str.charAt(0) === '/')
