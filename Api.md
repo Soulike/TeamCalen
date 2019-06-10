9c3d9827bacda8bfe6d397ec67709c59ad17babb
Soulike <316413310@qq.com>
2019-06-10 09:30:53 +0800

    对应数据库修改 UserProfile 类

--- Api.md
+++ Api.md
@@ -107,13 +107,7 @@ class UserProfile
     public avatar?: Buffer | null;
     public motto?: string | null;
     public username?: string | null;
-
-    constructor(username?: string | null, avatar?: Buffer | null, motto?: string | null)
-    {
-        this.username = username;
-        this.avatar = avatar;
-        this.motto = motto;
-    }
+    public email?:string | null;
 }
 ```
 
--- src/Class/UserProfile.ts
+++ src/Class/UserProfile.ts
@@ -3,11 +3,13 @@ export class UserProfile
     public avatar?: Buffer | null;
     public motto?: string | null;
     public username?: string | null;
+    public email?: string | null;
 
-    constructor(username?: string | null, avatar?: Buffer | null, motto?: string | null)
+    constructor(username?: string | null, avatar?: Buffer | null, motto?: string | null, email?: string | null)
     {
         this.username = username;
         this.avatar = avatar;
         this.motto = motto;
+        this.email = email;
     }
 }
